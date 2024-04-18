5f0115e486f9ca45782c3d50d9cb3275d0dbb088
trikhonghu <113248981+trikhonghu@users.noreply.github.com>
2024-04-18 09:49:47 +0700

    "parameter change"

--- gateway/my_parameters.py
+++ gateway/my_parameters.py
@@ -1,10 +1,24 @@
+from my_crc import CRC_MOD_BUS
 
 crc_lists = {
-    "Phong": [1,2,3,4,5]
+    "mixer1_ON": [1, 6, 0, 0, 0, 255],
+    "mixer1_OFF": [1, 6, 0, 0, 0, 0],
+    "mixer2_ON": [2, 6, 0, 0, 0, 255],
+    "mixer2_OFF": [2, 6, 0, 0, 0, 0],
+    "mixer3_ON": [3, 6, 0, 0, 0, 255],
+    "mixer3_OFF": [3, 6, 0, 0, 0, 0],
+    "selector1_ON": [4, 6, 0, 0, 0, 255],
+    "selector1_OFF": [4, 6, 0, 0, 0, 0],
+    "selector2_ON": [5, 6, 0, 0, 0, 255],
+    "selector2_OFF": [5, 6, 0, 0, 0, 0],
+    "selector3_ON": [6, 6, 0, 0, 0, 255],
+    "selector3_OFF": [6, 6, 0, 0, 0, 0],
+    "pump_in_ON": [7, 6, 0, 0, 0, 255],
+    "pump_in_OFF": [7, 6, 0, 0, 0, 0],
+    "pump_out_ON": [8, 6, 0, 0, 0, 255],
+    "pump_out_OFF": [8, 6, 0, 0, 0, 0],
+    "soil_temperature":[10 ,3, 0, 6, 0, 1],
+    "soil_humidity": [10, 3, 0, 7, 0, 1]
 }
 
-
-
-
-
-print(crc_lists["Phong"])
+crc_calc = CRC_MOD_BUS(crc_lists)
