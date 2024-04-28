b0e7d57642dd572a038ac8f3a7cc06436fffc3cd
kido2k3 <hoangphongvip2003@gmail.com>
2024-04-28 14:37:52 +0700

    add tool bar, task list

--- appMobi/lib/main_screen.dart
+++ appMobi/lib/main_screen.dart
@@ -1,6 +1,9 @@
 
 
 import 'package:app_mobi/home_page/home_page.dart';
+import 'package:app_mobi/main.dart';
+import 'package:app_mobi/my_share/background.dart';
+import 'package:app_mobi/my_share/my_text_style.dart';
 import 'package:flutter/material.dart';
 
 class MainScreen extends StatelessWidget{
@@ -8,8 +11,18 @@ class MainScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return const Scaffold(
-      body: SafeArea(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("SCHEDULING APP"),
+        centerTitle: true,
+        titleTextStyle: const TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+          fontSize: 30
+        ),
+        backgroundColor: Colors.green
+      ),
+      body: const SafeArea(
         child: HomePage(),
       ),
     );
--- appMobi/lib/my_share/my_text_style.dart
+++ appMobi/lib/my_share/my_text_style.dart
@@ -1,14 +1,20 @@
 
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 
-class MyTextStyle{
+class MyTextStyle {
   static TextStyle weather = const TextStyle(
     color: Colors.black,
     fontSize: 18,
     fontFamily: 'Inter',
     fontWeight: FontWeight.w500,
+    height: 0,
+    letterSpacing: -0.24,
+  );
+  static TextStyle welcome = const TextStyle(
+    color: Colors.black,
+    fontSize: 30,
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w600,
     height: 0,
     letterSpacing: -0.24,
   );
