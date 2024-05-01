f9a3c2940f30d2190a419b843be5835d54556c89
kido2k3 <hoangphongvip2003@gmail.com>
2024-05-01 11:21:46 +0700

    change format

--- gateway/my_task.py
+++ gateway/my_task.py
@@ -1,6 +1,8 @@
+from my_parameters import *
+
 
 class Task:
-    def __init__(self, id:str, cycle_num:int, mixer: list, area:str, start_time:str) -> None:
+    def __init__(self, id: str, cycle_num: int, mixer: list, area: str, start_time: str) -> None:
         if len(mixer) == 3:
             self.id = id
             self.cycle_num = cycle_num
@@ -9,13 +11,17 @@ class Task:
             self.start_time = start_time
         else:
             print("invalid mixer")
+
     def __str__(self) -> str:
         return f"Task {self.id}, {self.cycle_num} cycles, {self.mixer}(mixer1, 2, 3), area {self.area}, {self.start_time}"
 
+
 class Task_List:
     task_list = list()
+
     def is_empty(self):
         return len(self.task_list) == 0
+
     def add(self, task: Task):
         if self.is_empty():
             self.task_list.append(task)
@@ -26,11 +32,34 @@ class Task_List:
                 return
             if i == len(self.task_list) - 1:
                 self.task_list.append(task)
+
     def __str__(self) -> str:
         string = str()
         for task in self.task_list:
             string += (str(task)+'\n')
         return string
+
+
+waiting = Task_List()
+running = Task_List()
+status = WAITING
+
+
+def check_waiting_task():
+    # check the time of tasks in waiting list
+    # if there is any in waiting list need to be run, remove that away the list, add that in running
+    global waiting, running
+    pass
+
+
+def check_running_task():
+    global running, status
+# check running list is empty or not.
+# If running list is not empty, run task index = 0, decrease cycle, remove and
+# re-add that task if cycle is not equal 0
+
+    pass
+
 
 # for testing
 # from datetime import datetime
