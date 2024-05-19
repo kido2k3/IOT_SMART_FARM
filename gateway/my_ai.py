f6896e00574fdec10352f85db82d6482d6efad95
kido2k3 <hoangphongvip2003@gmail.com>
2024-05-19 15:10:17 +0700

    testing ai

--- gateway/my_ai.py
+++ gateway/my_ai.py
@@ -4,9 +4,8 @@
 # print('Shape of data',df.shape)
 # df.head()
 # df
-# from pmdarima import auto_arima
-# stepwise_fit = auto_arima(df['Temp'], trace=True,
-# suppress_warnings=True)
+#
+
 # print(df.shape)
 # train=df.iloc[:-30]
 # test=df.iloc[-30:]
@@ -16,41 +15,75 @@
 # model=ARIMA(train['Temp'],order=(1,0,5))
 # model=model.fit()
 # model.summary()
-
 import matplotlib.pyplot as plt
 import pandas as pd
 from statsmodels.tsa.arima.model import ARIMA
+from pmdarima import auto_arima
 
 
-# Read the CSV file
-csv_file = 'data.csv'  # Path to your CSV file
-data = pd.read_csv(csv_file, parse_dates=['Date'], index_col='Date')
+class ForecastModel:
+    def __init__(self, csv_file, name) -> None:
+        # Read the CSV file
+        self.csv_file = csv_file  # Path to your CSV file
+        self.data = pd.read_csv(csv_file, parse_dates=[
+                                'Date'], index_col='Date')
+        self.name = name
+
+    def determine_parameter(self):
+        stepwise_fit = auto_arima(
+            self.data[self.name], trace=True,  suppress_warnings=True)
+        print(stepwise_fit)
+
+    def create_model(self, p, d, q):
+        # Fit the ARIMA model
+        model = ARIMA(self.data[self.name], order=(p, d, q))
+        self.model_fit = model.fit()
+
+    def forecasting(self, forecast_steps):
+        # Forecast the next 15 seconds
+        self.forecast = self.model_fit.forecast(steps=forecast_steps)
+
+    def draw_figure(self, forecast_steps, save_in="D:\\phong.png"):
+        # Create a new time index for the forecast
+        forecast_index = pd.date_range(
+            start=self.data.index[-1], periods=forecast_steps + 1, freq='S')[1:]
+        # Plot the results
+        plt.figure(figsize=(10, 5))
+        plt.plot(self.data.index, self.data['Temp'], label='Observed')
+        plt.plot(forecast_index, self.forecast, label='Forecast', color='red')
+        plt.xlabel('Time')
+        plt.ylabel(f'{self.name}')
+        plt.title(f'{self.name} Forecast for the {forecast_steps} Next Seconds')
+        plt.legend()
+        plt.savefig(f'{save_in}')
+        plt.show(block=False)
+
+    def verification(self, value):
+        # Print the forecasted values
+        predicted_data = self.forecast.to_list()[0]
+        print("Predicted data: ", predicted_data, "-------")
+        if predicted_data > value:
+            percentage = value/predicted_data
+        else:
+            percentage = predicted_data/value
+        if percentage < 0.5:
+            print(
+                f"Percentage of {self.name} forecasting is too low, please recheck sensors or model")
+
+    def retrain(self):
+        self.create_model(0, 1, 0)
 
 
-# Fit the ARIMA model
-p, d, q = 0, 1, 0  # Example parameters, you might need to tune these
-model = ARIMA(data['Temp'], order=(p, d, q))
-model_fit = model.fit()
+temperature_model = ForecastModel("data.csv", "Temp")
+# temperature_model.determine_parameter("Temp")
+temperature_model.create_model(0, 1, 0)
+temperature_model.forecasting(15)
+temperature_model.draw_figure(15)
+temperature_model.verification(1)
 
-# Forecast the next 15 seconds
-forecast_steps = 225
-forecast = model_fit.forecast(steps=forecast_steps)
-
-# Create a new time index for the forecast
-forecast_index = pd.date_range(
-    start=data.index[-1], periods=forecast_steps + 1, freq='S')[1:]
-
-# Plot the results
-plt.figure(figsize=(10, 5))
-plt.plot(data.index, data['Temp'], label='Observed')
-plt.plot(forecast_index, forecast, label='Forecast', color='red')
-plt.xlabel('Time')
-plt.ylabel('Temperature')
-plt.title('Temperature Forecast for the Next 15 Seconds')
-plt.legend()
-plt.savefig('D:\\phong.png')
-plt.show(block=False)
-
-# Print the forecasted values
-predicted_data = forecast.to_list()
-print("Predicted data: ", predicted_data[0], "-------")
+humidity_model = ForecastModel("data.csv", "Humid")
+# humidity_model.determine_parameter("Humid")
+humidity_model.create_model(0, 1, 0)
+humidity_model.forecasting(15)
+humidity_model.draw_figure(15, "D:\\phong1.png")
+humidity_model.verification(20)
