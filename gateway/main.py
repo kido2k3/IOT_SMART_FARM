427a84af8bd91d4b0ef6293a1dde265682485648
trikhonghu <113248981+trikhonghu@users.noreply.github.com>
2024-05-19 16:34:14 +0700

    change

--- gateway/main.py
+++ gateway/main.py
@@ -7,8 +7,8 @@ if True:
     my_os.operation_system.add_process(my_task.check_waiting_task, 0, 5)
     my_os.operation_system.add_process(my_task.check_running_task, 0, 1)
     # 1 time / 5 seconds
-    my_os.operation_system.add_process(my_fsm.get_humidity, 20, 100)
-    my_os.operation_system.add_process(my_fsm.get_temperature, 0, 100)
+    # my_os.operation_system.add_process(my_fsm.get_humidity, 20, 100)
+    # my_os.operation_system.add_process(my_fsm.get_temperature, 0, 100)
 cnt = 0
 second = 0
 
@@ -18,8 +18,8 @@ while True:
         second+=1
         print(second)
         # print("status: ", status)
-        # print("wating task", waiting)
-        # print("running task", running)
+        # print("wating task", my_task.waiting)
+        # print("running task", my_task.running)
         # print(my_os.operation_system)
     else: cnt += 1
     my_os.operation_system.dispatch_process()
